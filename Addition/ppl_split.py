b055f6dac00adb9d482f9962bcd7d4068f7d0ded
ulyana-3107 <90414644+ulyana-3107@users.noreply.github.com>
2023-01-02 19:23:31 +0300

    Update ppl_split.py

--- Addition/ppl_split.py
+++ Addition/ppl_split.py
@@ -1,35 +1,30 @@
-def possible(n, matrix) -> bool:
+def possible(n, matrix) -> tuple:
     if n == 2:
-        return True
-    m = len(matrix[0])
-    all_ppl = set([i for i in range(1, n + 1)])
+        return [1], [2], True
+    m = n
+    all_ppl = set([i for i in range(n)])
     gr1, gr2, known = set(), set(), dict()
     for i in range(n):
         for j in range(m):
-            if matrix[i][j]:
+            if matrix[i][j] and i != j:
                 if i in known:
                     known[i].add(j)
                 else:
                     known[i] = {j}
     while all_ppl:
         p = all_ppl.pop()
-        if not len(gr1):
-            gr1.add(p)
-        elif not len(gr2):
-            gr2.add(p)
+        if p in known:
+            known_ = known[p]
         else:
-            if p in known:
-                known_ = known[p]
+            known_ = {}
+        if len(gr1 & known_):
+            if len(gr2 & known_):
+                return [i + 1 for i in gr1], [i + 1 for i in gr2], False
             else:
-                known_ = set()
-            if len(gr1 & known_):
-                if len(gr2 & known_):
-                    return False
-                else:
-                    gr2.add(p)
-            else:
-                gr1.add(p)
-    return True
+                gr2.add(p)
+        else:
+            gr1.add(p)
+    return [i + 1 for i in gr1], [i + 1 for i in gr2], True
 
 
 n1, m1 = 4, [[0, 1, 0, 1], [1, 0, 1, 1], [0, 1, 0, 1], [1, 1, 1, 0]]
@@ -37,7 +32,8 @@ n2, m2 = 4, [[0 for _ in range(4)] for _ in range(4)]
 n3, m3 = 5, [[0, 1, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 1, 1], [0, 0, 1, 0, 1], [0, 0, 1, 1, 0]]
 n4, m4 = 6, [[0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1],
              [1, 0, 1, 0, 1, 0]]
-n_, m_ = n1, m1
+n5, m5 = 5, [[0, 0, 0, 1, 1], [0, 0, 0, 1, 1], [0, 0, 0, 1, 1], [1, 1, 1, 0, 0], [1, 1, 1, 0, 0]]
+n_, m_ = n5, m5
 
 with open('ppl_split_input.txt', 'w', encoding='utf-8-sig') as in_writer:
     in_writer.write(str(n_) + '\n')
@@ -52,13 +48,10 @@ with open('ppl_split_input.txt', 'r', encoding='utf-8-sig') as reader:
         arr.append([int(i) for i in line.strip().split(' ')])
     print('Input is read.')
 
-result = 'Yes' if possible(_n, arr) else 'No'
+result = possible(_n, arr)
 print('Result is ready.')
 
 with open('ppl_split_output.txt', 'w', encoding='utf-8-sig') as out_writer:
-    out_writer.write(result)
+    for r in result:
+        out_writer.write(str(r) + '\n')
     print('Result is written.')
-
-
-
-

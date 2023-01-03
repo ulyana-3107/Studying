e22b9fe939bfa8ee3af838099966c4bab434c892
Ulyana <ulasevich.2003@inbox.ru>
2023-01-03 18:47:44 +0300

    complexity evaluated

--- DS/graphs/road_build.py
+++ DS/graphs/road_build.py
@@ -1,29 +1,37 @@
-def road_building(n, edges) -> str:
-    addr, components, comp_num = {i: i for i in range(1, n + 1)}, {i: [i] for i in range(1, n + 1)}, ''
-    for e in edges:
-        town_a, town_b = e
-        town_a_addr, town_b_addr = addr[town_a], addr[town_b]
-        if town_a_addr == town_b_addr:
-            comp_num += str(n) + ' '
+def road_building(n, edges) -> str:  # N, E - n, len(edges)
+    addr, components, comp_num = {i: i for i in range(1, n + 1)}, {i: [i] for i in range(1, n + 1)}, ''  # O(N)
+    for e in edges:  # O(E)
+        town_a, town_b = e  # O(1)
+        town_a_addr, town_b_addr = addr[town_a], addr[town_b]  # O(1)
+        if town_a_addr == town_b_addr:  # O(1)
+            comp_num += str(n) + ' '  # O(1)
         else:
-            size_a, size_b = len(components[town_a_addr]), len(components[town_b_addr])
-            a, b = size_a >= size_b, size_b > size_a
-            if a:
-                saved_towns = components[town_b_addr]
-                components[town_a_addr].extend(saved_towns)
-                for s_t in saved_towns:
-                    addr[s_t] = town_a_addr
-                components[town_b_addr] = []
+            size_a, size_b = len(components[town_a_addr]), len(components[town_b_addr])  # O(1)
+            a, b = size_a >= size_b, size_b > size_a  # O(1)
+            if a:  # O(1)
+                saved_towns = components[town_b_addr]  # O(1)
+                components[town_a_addr].extend(saved_towns)  # O(len(saved_towns)) -> O(N - 1) -> O(N)
+                for s_t in saved_towns:  # O(N)
+                    addr[s_t] = town_a_addr  # O(1)
+                components[town_b_addr] = []  # O(1)
             else:
-                saved_towns = components[town_a_addr]
-                components[town_b_addr].extend(saved_towns)
-                for s_t in saved_towns:
-                    addr[s_t] = town_b_addr
-                components[town_a_addr] = []
-            if n > 1:
-                n -= 1
-            comp_num += str(n) + ' '
+                saved_towns = components[town_a_addr]   # O(1)
+                components[town_b_addr].extend(saved_towns)  # O(len(saved_towns)) -> O(N - 1) -> O(N)
+                for s_t in saved_towns:  # O(N)
+                    addr[s_t] = town_b_addr  # O(1)
+                components[town_a_addr] = []  # O(1)
+            if n > 1:  # O(1)
+                n -= 1  # O(1)
+            comp_num += str(n) + ' '  # O(1)
     return comp_num
+
+# 4 - 5:  O(1)
+# 12 - 16: O(1) + O(N) + O(N)*O(1) + O(1) -> O(N)
+# 18 - 22: O(1) + O(N) + O(N)*O(1) + O(1) -> O(N)
+# 9 - 25: O(1) + O(1) + max(O(N), O(N)) + O(1) + O(1) + O(1) -> O(N)
+# 6 - 25: O(1) + max(O(1), O(N)) -> O(1) + O(N) -> O(N)
+# 3 - 26: O(E) * O(N) -> O(N*E)
+# RESULT: O(NE)
 
 # n, q, edges = 4, 3, [(1, 2), (3, 4), (2, 3)]  # 3 2 1
 # n, q, edges = 7, 6, [[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 1]]  # 6 5 4 3 2 2

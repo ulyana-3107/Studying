fbe9014129c10395d1e5305c7ef39bc9f3801103
ulyana-3107 <90414644+ulyana-3107@users.noreply.github.com>
2023-03-03 17:31:40 +0300

    new Regular expression

---
+++ regex1a.py
@@ -0,0 +1,30 @@
+import re
+
+
+def find_links(text: str) -> list:
+    pat = r'<a.*?(?:id=["\']\d+["\'])?.*?href=["\'](.+?)[\'"].*?(?:id=[\'"]\d+[\'"])?.*?</a>'
+    return re.findall(pat, text, flags=re.DOTALL)
+
+
+text = '''
+Blablablablablablabal
+<a
+ id="111" href="Link1"></a>
+<a id="222" href="Link2"></a>
+<a id="333" class="my_class" href="Link3"></a>
+<a id="444" href='Link4'></a>
+<a href="Link5" id="555">Some text</a>
+blablabla'''
+
+with open('html1a.txt', 'w', encoding='utf-8-sig') as input_file:
+    input_file.write(text)
+
+with open('html1a.txt', 'r', encoding='utf-8-sig') as input_reader2:
+    text = ''
+    for l in input_reader2.readlines():
+        text += l
+    links = find_links(text)
+
+with open('links1a.txt', 'w', encoding='utf-8-sig') as writer:
+    for l in links:
+        writer.write(l + '\n')

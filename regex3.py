3378d93623fbdd9f197793c8304ef5995d07b21b
ulyana-3107 <90414644+ulyana-3107@users.noreply.github.com>
2023-03-06 12:32:17 +0300

    new regular expression

---
+++ regex3.py
@@ -0,0 +1,32 @@
+import re
+
+
+# Удалить теги div, внутри них нет тега span
+
+# pat = r'<div.*?(?:<div>.+?</div>)*.*</div>'
+def del_div(text: str) -> str:
+    pat2 = r'^<div(?:(?!<span).)*?</div>$'
+    return re.sub(pat2, '', text, flags=re.MULTILINE)
+
+
+text = '''
+<div id="leftcol"><div>Test</div> Some text</div>
+    <div><div id="tagcloud">
+        <span class="mytags"><a href="">tag1</a></span>
+        <span class=""mytags""><a href="">tag2</a></span>
+        <!-- and a few more spans of the same type -->
+    </div>
+    Some text</div>"'''
+
+
+with open('html_3.txt', 'w', encoding='utf-8-sig') as writer:
+    writer.write(text)
+
+with open('html_3.txt', 'r', encoding='utf-8-sig') as reader:
+    text = ''
+    for l in reader.readlines():
+        text += l
+
+with open('html_3_result.txt', 'w', encoding='utf-8-sig') as writer:
+    writer.write(del_div(text))
+

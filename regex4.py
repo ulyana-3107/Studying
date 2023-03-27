5bfa3188f33fbe65800ced5f71c194a7151041e7
ulyana-3107 <90414644+ulyana-3107@users.noreply.github.com>
2023-03-27 14:14:56 +0300

    regular expression edited

---
+++ regex4.py
@@ -0,0 +1,28 @@
+import re
+
+
+#  Дан html-файл. Заменить теги <div> на <span>, если их id=”spannable”, а длина содержимого не больше 20 символов.
+
+
+def div_span(text: str) -> str:
+    pat = r'(?:(.*?<div.*id=[\'"]spannable[\'"].*?>)(.{,20})(.*</div>))'
+    return re.sub(pat, r'<span>\2</span>', text)
+
+
+text = '''
+<div id="spannable">hello</div>
+    <div id="tagcloud"></div>
+    <div id="spannable"><a href="""">tag1</a></div>
+'''
+
+with open('html_4.txt', 'w', encoding='utf-8-sig') as writer:
+    writer.write(text)
+
+with open('html_4.txt', 'r', encoding='utf-8-sig') as reader:
+    text = ''
+    for l in reader.readlines():
+        text += l
+
+with open('html_4_results.txt', 'w', encoding='utf-8-sig') as writer:
+    result = div_span(text)
+    writer.write(result)

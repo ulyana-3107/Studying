08e6bf2b476395660bb6d736f66c91f13ebcc13d
Kuznetsov Andrei <ulasevich.2003@inbox.ru>
2023-04-12 14:06:47 +0300

    friends task edited

--- can_divide.py
+++ can_divide.py
@@ -1,100 +1,111 @@
-def create_dict(m: list, n: int) -> dict:
-    d = {}
+from collections import deque
+# Алгоритм:
+# 1. если каждый человек знает не больше 1го - ответ Yes.
+# 2. выбираем человека с максимальным количеством знакомых и помещаем его в группу номер 1
+# 3. берем всех его знакомых и помещаем их в группу номер 2
+# 4. проходимся по людям из второй группы: создаем список всех знакомых тут, затем:
+# если его нет в первой группе, проверяем нет ли там общих знакомых, если нет - добавляем, если есть - ответ No.
+# 5. в порядке убывания по количеству знакомых проходимся по всем остальным, проделывая те же шаги.
+
+
+def create_data(matrix: list) -> tuple:
+    friends, friends_num, n = {}, {}, len(matrix)
     for i in range(n):
-        d[i + 1] = set([j + 1 for j in range(n) if m[i][j] == 1 and i != j])
-    return d
+        friends[i] = set()
+        for j in range(n):
+            if matrix[i][j] and i != j:
+                friends[i].add(j)
+        friends_num[i] = len(friends[i])
+    return friends, friends_num
 
 
-def dividible(matrix: list, n: int) -> tuple:
-    if n < 3:
-        return True, {1}, {2}
-    known, ppl = create_dict(matrix, n), [False for i in range(n)]
-    g1, g2 = set(), set()
-    for i in range(n):
-        if len(known[i + 1]) > 0:
-            p = known[i + 1].pop()
-            g1.add(i + 1)
-            g2.add(p)
-            ppl[i], ppl[p - 1] = True, True
-            known[i + 1].add(p)
-            break
-    if len(g1) == 0:
-        return 'Yes', [i + 1 for i in range(n//2)], [i + 1 for i in range(n//2, n)]
-    temp = set()
-    for i in range(n):
-        if not ppl[i]:
-            p = i + 1
-            nbrs = known[p]
-            if not len(nbrs):
-                g1.add(p)
-                ppl[i] = True
-            else:
-                a, b = nbrs & g1, nbrs & g2
-                if len(a) > 0 and len(b) == 0:
-                    g2.add(p)
-                    ppl[p - 1] = True
-                elif len(b) > 0 and len(a) == 0:
-                    g1.add(p)
-                    ppl[p - 1] = True
-                elif len(a) > 0 and len(b) > 0:
-                    return 'No', set(), set()
-                else:
-                    temp.add(p)
-    if not len(temp):
-        return 'Yes', g1, g2
-    else:
-        g1_, g2_ = set(), set()
-        temp_, c = set(), 0
-        while len(temp):
-            p1 = temp.pop()
-            nbrs = known[p1]
-            if c == 0:
-                p2 = nbrs.pop()
-                temp -= {p2}
-                g1_.add(p1)
-                g2_.add(p2)
-                ppl[p1 - 1], ppl[p2 - 1] = True, True
-                c += 1
-            else:
-                a, b = nbrs & g1_, nbrs & g2_
-                if not len(a) and not len(b) or len(a) and len(b):
-                    temp_.add(p1)
-                else:
-                    if len(b) and not len(a):
-                        g1_.add(p1)
-                    elif len(a) and not len(b):
-                        g2_.add(p1)
-                    ppl[p1 - 1] = True
-        if not len(temp_):
-            g1.update(g1_)
-            g2.update(g2_)
-            return 'Yes', g1, g2
-        else:
-            return 'No', set(), set()
+def friends_true(s: set, friends: dict) -> bool:
+    for elem in s:
+        if len(friends[elem]&s):
+            return True
+    return False
 
 
-m = [[1, 0, 1, 0], [0, 1, 0, 1], [1, 0, 1, 1], [0, 1, 1, 1]]  # True
-m1 = [[1, 0, 0, 1, 1], [0, 1, 0, 1, 1], [0, 0, 1, 1, 1], [1, 1, 1, 1, 0], [1, 1, 1, 0, 1]]  # True
-m2 = [[0, 1, 0, 0, 0], [1, 0, 0, 0, 0], [0, 0, 0, 1, 1], [0, 0, 1, 0, 1], [0, 0, 1, 1, 0]]  # False
-m3 = [[1, 0, 1, 0, 0], [0, 1, 0, 0, 1], [1, 0, 1, 0, 0], [0, 0, 0, 1, 1], [0, 1, 0, 1, 0]]
+def dividible(matrix: list):
+    distributed, gr1, gr2 = set(), set(), set()
+    friends, friends_num = create_data(matrix)
+    sorted_friends_num = {}
+    sorted_d = sorted(friends_num, key=friends_num.get)
+    sorted_d.reverse()
+    if sorted_d[0] < 2:
+        return 'Yes'
+    for elem in sorted_d:
+        if elem not in distributed and not len(gr1):
+            distributed.add(elem)
+            gr1.add(elem)
+            friends_ = friends[elem]
+            gr2 = gr2.union(friends_)
+            if friends_true(gr2, friends):
+                return 'No', None, None
+            distributed = distributed.union(gr2)
+            friends_2 = []
+            for elem in gr2:
+                friends_2.extend(list(friends[elem]))
+            for f in friends_2:
+                if f not in gr1:
+                    if not len(gr1 & friends[f]):
+                        gr1.add(f)
+                        distributed.add(f)
+                    else:
+                        return 'No', None, None
+        elif elem not in distributed:
+            if not len(gr1 & friends[elem]):
+                gr1.add(elem)
+                distributed.add(elem)
+            elif not len(gr2 & friends[elem]):
+                gr2.add(elem)
+                distributed.add(elem)
+    gr1, gr2 = [i + 1 for i in list(gr1)], [i + 1 for i in list(gr2)]
+    return 'Yes', gr1, gr2
 
-with open('acquaintances.input.txt', 'w', encoding='utf-8-sig') as writer:
-    writer.write(str(len(m3)) + '\n')
-    for arr in m3:
-        writer.write(' '.join([str(i) for i in arr]) + '\n')
+# matrix = [
+#           [1, 0, 0, 0, 0, 1],
+#           [0, 1, 0, 1, 0, 0],
+#           [0, 0, 1, 0, 1, 0],
+#           [0, 1, 0, 1, 1, 0],
+#           [0, 0, 1, 1, 1, 0],
+#           [1, 0, 0, 0, 0, 1]  # Yes
+#          ]
+# matrix = [[0, 1, 0, 0, 0],
+#           [1, 0, 0, 0, 0],
+#           [0, 0, 0, 1, 1],
+#           [0, 0, 1, 0, 1],
+#           [0, 0, 1, 1, 0]]  # No
+# matrix = [[1, 0, 0, 1, 1],
+#           [0, 1, 0, 1, 1],
+#           [0, 0, 1, 1, 1],
+#           [1, 1, 1, 1, 0],
+#           [1, 1, 1, 0, 1]]  # Yes
+# matrix = [[1, 0, 1, 0, 0],
+#           [0, 1, 0, 0, 1],
+#           [1, 0, 1, 0, 0],
+#           [0, 0, 0, 1, 1],
+#           [0, 1, 0, 1, 0]]  # Yes
 
-with open('acquaintances.input.txt', 'r', encoding='utf-8-sig') as reader:
-    num = int(reader.readline().strip())
-    matrix = []
+
+matrix = [[1, 1, 0, 0],
+          [1, 1, 0, 0],
+          [0, 0, 1, 1],
+          [0, 0, 1, 1]]  # Yes
+with open('friends_matrix.txt', 'w', encoding = 'utf-8-sig') as writer:
+    for line in matrix:
+        writer.write(' '.join([str(i) for i in line]) + '\n')
+
+with open('friends_matrix.txt', 'r', encoding='utf-8-sig') as reader:
+    m = []
     for line in reader.readlines():
-        matrix.append([int(i) for i in line.strip().split(' ')])
+        m.append([int(i) for i in line.strip().split(' ')])
 
-with open('acquaintances.result.txt', 'w', encoding='utf-8-sig') as writer:
-    bool_res, gr1, gr2 = dividible(matrix, num)
-    writer.write(str(bool_res) + '\n')
-    writer.write('Group 1: ' + ','.join([str(i) for i in gr1]) + '\n')
-    writer.write('Group 2: ' + ','.join([str(i) for i in gr2]) + '\n')
+result = dividible(m)
 
-
-
-
+with open('friends_result.txt', 'w', encoding='utf-8-sig') as writer:
+    if type(result) == str:
+        writer.write(result)
+    else:
+        for elem in result:
+            writer.write(str(elem) + '\n')

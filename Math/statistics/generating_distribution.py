ef97b30633d67204a5e6e8a992bd47f194c6fd75
Kuznetsov Andrei <ulasevich.2003@inbox.ru>
2023-04-30 13:39:25 +0300

    script edited

--- Math/statistics/generating_distribution.py
+++ Math/statistics/generating_distribution.py
@@ -1,29 +1,42 @@
 import os
 import numpy
+import argparse
 
 
-# Генерация выборки нормального распределения размером N и запись в файл.
-N = 10
-normal = numpy.random.normal(size=N)
-
-with open('../normal_distribution.txt', 'w', encoding='utf-8-sig') as writer:
-    writer.write('Normal\n')
-    writer.write(','.join([str(n) for n in normal]))
+# Генерация выборки нормального распределения размером size
+def generate_normal_dist(size: int):
+    normal = numpy.random.normal(size=size)
+    return normal
 
 
-# Генерация выборки равномерного распределения размером N и запись в файл.
-N = 10
-uniform = numpy.random.uniform(size=N)
-
-with open('../uniform_distribution.txt', 'w', encoding='utf-8-sig') as writer:
-    writer.write('Uniform\n')
-    writer.write(','.join([str(n) for n in uniform]))
+# Генерация выборки равномерного распределения размером size
+def generate_uniform_dist(size: int):
+    uniform = numpy.random.uniform(size=size)
+    return uniform
 
 
-# Генерация выборки экспоненциального распределения размером N и запись в файл.
-N = 10
-exponential = numpy.random.exponential(size=N)
+# Генерация выборки экспоненциального распределения размером size
+def generate_exponential_dist(size: int):
+    exponential = numpy.random.exponential(size=size)
+    return exponential
 
-with open('../exponential_distribution.txt', 'w', encoding='utf-8-sig') as writer:
-    writer.write('Exponential\n')
-    writer.write(','.join([str(n) for n in exponential]))
+
+def generate_and_write(d_type: str, path: str, size: int):
+    distribution = eval(f'generate_{d_type.strip().lower()}_dist')(size)
+
+    with open(path, 'w', encoding='utf-8-sig') as writer:
+        writer.write(d_type + '\n')
+        writer.write(','.join([str(d) for d in distribution]))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Generating distribution')
+    parser.add_argument('dist_type', type=str, help='type of distribution')
+    parser.add_argument('path', type=str, help='path to the file with generated distribution')
+    parser.add_argument('-s', '--size', type=int, default=10, help='size of a distribution, (default=10)')
+    arguments = parser.parse_args()
+
+    generate_and_write(arguments.dist_type, arguments.path, arguments.size)
+
+

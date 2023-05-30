913ed73cf0c01c3ea0878d9d9b7f7afd6465bd3f
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-05-30 14:14:52 +0300

    combinations edited

--- Recursion/combinations.py
+++ Recursion/combinations.py
@@ -1,17 +1,17 @@
-def combinations(lst, n):
+def combinations(lst, ind, n):
     if n == 0:
         return [[]]
     l = []
-    for j in range(len(lst)):
+    for j in range(ind, len(lst)):
         empty = lst[j]
-        rec_lst = lst[j + 1:]
 
-        for x in combinations(rec_lst, n - 1):
+        for x in combinations(lst, ind,  n - 1):
             l.append([empty] + x)
+        ind += 1
 
     return l
 
 
 if __name__ == '__main__':
     array = list(range(1, 6))
-    print(combinations(array, 3))
+    print(combinations(array, 0, 3))

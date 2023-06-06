39b02e1c03f73b0afd9f25f1349f7b6778ebe5b9
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-06-06 14:53:15 +0300

    chaned encryption algorithm, added decrypt parameter

--- Algorithms/encryption/shift_enc.py
+++ Algorithms/encryption/shift_enc.py
@@ -8,24 +8,18 @@ import argparse
 from pathlib import Path
 
 
-def read_text(path: str) -> str:
-    if Path(path).exists:
-        return Path(path).read_text()
-
-    raise FileNotFoundError()
-
-
-def shift_encrypt(path: str, dst_path: str, key: str) -> None:
+def encrypt(path: str, dst_path: str, key: str, decrypt: bool = False) -> None:
     if not Path(path).exists():
         raise FileExistsError()
 
-    text = Path(path).read_text()
-
-    indxs, text2 = {}, ''
+    text = Path(path).read_text('utf-8-sig')
+    indxs_txt, indxs_key, text2 = {}, {}, ''
 
     for i in range(len(letters)):
         if letters[i] in text or letters[i].upper() in text:
-            indxs[letters[i].lower()] = i
+            indxs_txt[letters[i].lower()] = i
+        if letters[i] in key or letters[i].upper() in key:
+            indxs_key[letters[i].lower()] = i
 
     i, c = 0, 0
     while i < len(text):
@@ -36,8 +30,8 @@ def shift_encrypt(path: str, dst_path: str, key: str) -> None:
 
             if c == len(key):
                 c = 0
-            shift = c + 1
-            index = indxs[text[i].lower()] + shift
+            shift = indxs_key[key[c]] if not decrypt else -indxs_key[key[c]]
+            index = indxs_txt[text[i].lower()] + shift
 
             if index >= len(letters):
                 index = index - len(letters)
@@ -57,47 +51,6 @@ def shift_encrypt(path: str, dst_path: str, key: str) -> None:
         writer.write(text2)
 
 
-def decrypt_from_shift(path: str, dst_path: str, key: str) -> None:
-    if not Path(path).exists():
-        raise FileExistsError()
-
-    text = Path(path).read_text()
-
-    len_nums = len([elem for elem in text if elem.lower() in letters])
-    real_text, indxs = '', {}
-
-    for i in range(len(letters)):
-        if letters[i] in text or letters[i].upper() in text:
-            indxs[letters[i]] = i
-
-    i = len(text) - 1
-    if len_nums % len(key):
-        shift = len_nums % len(key)
-    else:
-        shift = len(key)
-
-    while i > -1:
-        if text[i].lower() in letters:
-            lowered = False
-            if text[i].lower() != text[i]:
-                lowered = True
-            shift = len(key) if shift == 0 else shift
-            index = indxs[text[i].lower()] - shift
-            let2 = letters[index] if not lowered else letters[index].upper()
-            shift -= 1
-        else:
-            let2 = text[i]
-
-        i -= 1
-        real_text = let2 + real_text
-
-    if not Path(dst_path).exists():
-        Path(dst_path).touch()
-
-    with open(dst_path, 'w', encoding='utf-8-sig') as writer:
-        writer.write(real_text)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Shift encryption realisation')
     parser.add_argument('path', type=str, help='path to the file with text')
@@ -105,7 +58,4 @@ if __name__ == '__main__':
     parser.add_argument('key', type=str, help='key for encryption')
     parser.add_argument('-d', '--decrypt', type=bool, default=False, help='will decrypt text if True, else encrypt')
     args = parser.parse_args()
-    if args.decrypt:
-        decrypt_from_shift(args.path, args.dst_path,  args.key)
-    else:
-        shift_encrypt(args.path, args.dst_path, args.key)
+    encrypt(args.path, args.dst_path, args.key, args.decrypt)

01402dadb0968756458d6a0ab3a8484b4158c29d
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-06-06 14:28:12 +0300

    extra code deleted, added decrypt parameter

--- Algorithms/encryption/caesar.py
+++ Algorithms/encryption/caesar.py
@@ -8,23 +8,18 @@ from string import ascii_lowercase as letters
 import argparse
 
 
-def read_text(file_path: str) -> str:
-    if Path(file_path).exists:
-        return Path(file_path).read_text()
-
-    raise FileNotFoundError()
-
-
-def caesar_encrypt(path: str, dst_path: str, shift: int = 2) -> None:
+def encrypt(path: str, dst_path: str, decrypt: bool = False, shift: int = 2) -> None:
     if not Path(path).exists:
         raise FileExistsError()
 
-    text = Path(path).read_text()
+    text = Path(path).read_text('utf-8-sig')
     indxs, text2 = {}, ''
 
     for i in range(len(letters)):
-        if letters[i] in text or letters[i].upper():
+        if letters[i] in text or letters[i].upper() in text:
             indxs[letters[i]] = i
+    if decrypt:
+        shift = -shift
 
     for let in text:
 
@@ -51,48 +46,19 @@ def caesar_encrypt(path: str, dst_path: str, shift: int = 2) -> None:
         writer.write(text2)
 
 
-def decrypt_from_caesar(path: str, dst_path: str, shift: int = 2) -> None:
-    if not Path(path).exists():
-        raise FileExistsError()
-
-    text = Path(path).read_text()
-
-    indxs, real_text = {}, ''
-    for i in range(len(letters)):
-        if letters[i] in text or letters[i].upper() in text:
-            indxs[letters[i]] = i
-
-    for let in text:
-        if let.lower() in letters:
-            lowered = False
-            if let.lower() != let:
-                lowered = True
-
-            index = indxs[let.lower()] - shift
-            let2 = letters[index] if not lowered else letters[index].upper()
-
-        else:
-            let2 = let
-        real_text += let2
-
-    if not Path(dst_path).exists():
-        Path(dst_path).touch()
-
-    with open(dst_path, 'w', encoding='utf-8-sig') as writer:
-        writer.write(real_text)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ceasar encryption')
     parser.add_argument('path', type=str, help='path to the file with text')
     parser.add_argument('dst_path', type=str, help = 'path for the en/de_crypted text to be written')
-    parser.add_argument('-sh', '--shift', type=int, default=2, help='number of indexes to shift letters')
     parser.add_argument('-dec', '--decrypt', type=bool, default=False, help='will decrypt text if True, else encrypt')
+    parser.add_argument('-sh', '--shift', type=int, default=2, help='number of indexes to shift letters')
     args = parser.parse_args()
-    if args.decrypt:
-        decrypt_from_caesar(args.path, args.dst_path,  args.shift)
-    else:
-        caesar_encrypt(args.path, args.dst_path, args.shift)
+    encrypt(args.path, args.dst_path, args.decrypt, args.shift)
+    # p1 = r'Algorithms\encryption\text.txt'
+    # p2 = r'res.txt'
+    # p3 = r'decrypted.txt'
+    # encrypt(p2, p3, True, 4)
+
 
 
 

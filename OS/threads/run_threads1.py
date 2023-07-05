1aa2b71e0247f48939cda516c4d7391a5a933706
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-07-05 19:15:35 +0300

    commit

--- OS/threads/run_threads1.py
+++ OS/threads/run_threads1.py
@@ -15,23 +15,22 @@ import os
 import argparse
 
 
-def func(res_queue):
+def func():
     name = inspect.currentframe().f_code.co_name
     print(f'{name} is running...')
     time.sleep(random.randint(1, 10))
     print(f'pid: {os.getpid()}')
-    res_queue.put(random.randint(1, 100))
 
 
 def start_processes(n: int) -> int:
     sum_of_codes = 0
 
     for i in range(n):
-        res_queue = multiprocessing.Queue()
-        process = multiprocessing.Process(target=func, args=(res_queue, ))
+        process = multiprocessing.Process(target=func)
         process.start()
         process.join()
-        sum_of_codes += res_queue.get()
+        if process.exitcode == 0:
+            sum_of_codes + random.randint(1, 100)
 
     return sum_of_codes
 
@@ -41,5 +40,5 @@ if __name__ == '__main__':
     parser.add_argument('N', type=int, help='Number of processes to run!')
     args = parser.parse_args()
     exit_codes = start_processes(args.N)
-    print(f'Sum of exit codes of all processes: {exit_codes}')
+    print(f'Sum of random exit codes of all processes: {exit_codes}')
 

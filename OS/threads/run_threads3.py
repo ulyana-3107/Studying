df75ce4e92619ed288fa827f65d48993f231cc8e
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-07-13 15:01:56 +0300

    edited

--- OS/threads/run_threads3.py
+++ OS/threads/run_threads3.py
@@ -9,28 +9,25 @@ import inspect
 import time
 import random
 import argparse
+import sys
 
 
 def func():
     time.sleep(random.randint(1, 10))
-    return os.getpid()
+    return
 
 
 def run_threads(num):
     pool, completed_count = multiprocessing.Pool(), 0
     async_results = [pool.apply_async(func) for _ in range(num)]
 
-    while completed_count < num:
-        chosen = random.choice(async_results)
-        while True:
-            if chosen.ready():
+    while async_results:
+        for i in async_results:
+            if i.ready():
                 completed_count += 1
-                async_results.remove(chosen)
-                pid = chosen.get()
-                print(f'Process with pid {pid} exited.')
+                print(f'{completed_count} processes finished.')
+                async_results.remove(i)
                 break
-
-            time.sleep(1)
 
 
 if __name__ == "__main__":

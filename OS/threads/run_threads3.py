bf530fd34d229a0a1de2cac7de35476ce725c519
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-07-20 14:45:04 +0300

    sleep time added, pool replaced with process

--- OS/threads/run_threads3.py
+++ OS/threads/run_threads3.py
@@ -18,16 +18,22 @@ def func():
 
 
 def run_threads(num):
-    pool, completed_count = multiprocessing.Pool(), 0
-    async_results = [pool.apply_async(func) for _ in range(num)]
+    completed_count = 0
+    async_results = []
+    for _ in range(num):
+        pr = multiprocessing.Process(target=func)
+        pr.start()
+        async_results.append(pr)
 
     while async_results:
         for i in async_results:
-            if i.ready():
+            if not i.is_alive():
                 completed_count += 1
                 print(f'{completed_count} processes finished.')
                 async_results.remove(i)
                 break
+
+            time.sleep(1.5)
 
 
 if __name__ == "__main__":

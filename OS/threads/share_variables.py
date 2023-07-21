f674c353c5bd36337baa97904c6658de72679718
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-07-21 14:37:17 +0300

    commit

--- OS/threads/share_variables.py
+++ OS/threads/share_variables.py
@@ -8,42 +8,53 @@
 import threading
 import random
 import time
+import multiprocessing as mp
 
 file_name = 'logs2.txt'
 file = open(file_name, 'w')
 
 
-def thread_function(thread_id, m, k, t):
-    mutex = threading.Lock()
+def thread_function(thread_id, k, t, mutex, values):
+    mutex.acquire()
 
     for _ in range(t):
 
-        acquired_variables = random.sample(range(m), k)
-
-        avg = sum(i for i in acquired_variables) / k
+        avg = sum(values) / len(values)
         mult = random.randint(1, 3)
-        result = avg * mult
+        indxs = [random.choice(range(len(values))) for i in range(k)]
+        acquired_variables = [values[i] for i in indxs]
+        for i in indxs:
+            values[i] = values[i] * mult
 
-        with mutex:
-            message = f'Thread {thread_id}: Acquired variables: {acquired_variables}, avg: {avg}, multiplied: {result}\n'
-            file.write(message)
+        message = f'Thread {thread_id}: Acquired variables: {acquired_variables}, avg: {avg}, multiplied: {values}\n'
+        file.write(message)
 
         time.sleep(random.randint(1, 5))
+
+    mutex.release()
 
     file.flush()
 
 
 if __name__ == '__main__':
-    threads = []
+    jobs = []
     n, m, k, t = 5, 3, 2, 3
+    common_vars = [random.choice([i for i in range(10)]) for _ in range(m)]
+    pid = 0
+
+    mutex = mp.Lock()
+    manager = mp.Manager()
+    vars = manager.list(common_vars)
 
     for i in range(n):
-        thread = threading.Thread(target=thread_function, args=(i, m, k, t))
-        thread.start()
-        threads.append(thread)
+        pid += 1
+        pr = mp.Process(target=thread_function, args=(pid, k, t, mutex, vars))
+        pr.start()
+        pr.join()
+        jobs.append(pr)
 
-    for thread in threads:
-        thread.join()
+    for proc in jobs:
+        proc.join()
 
     file.close()
 

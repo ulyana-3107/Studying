772f6e3ffe0d00626f776856d1fe5b8c52863ef8
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-07-28 16:06:02 +0300

    edited logging

--- OS/threads/share_variables.py
+++ OS/threads/share_variables.py
@@ -3,37 +3,47 @@
 # число (от 1 до 3) и перезаписывает их значения, спит случайное время (1-5 секунд) и снова повторяет свои
 # действия (делает так t раз). При всех действиях пусть пишет соответствующие сообщения (в файл).
 # Реализовать эту логику.
-
-
+import os
 import threading
 import random
 import time
 import multiprocessing as mp
 
-file_name = 'logs2.txt'
-file = open(file_name, 'w')
+
+def logger(queue, file_name):
+    file = open(file_name, 'a+')
+    while not queue.empty():
+        try:
+            msg = queue.get()
+            file.write(msg)
+            file.flush()
+        except:
+            time.sleep(0.2)
+
+    file.close()
 
 
-def thread_function(thread_id, k, t, mutex, values):
-    mutex.acquire()
+def thread_function(thread_id, k, t, mutex, values, q):
 
     for _ in range(t):
+        mutex.acquire()
 
-        avg = sum(values) / len(values)
+        indxs = [random.choice(range(len(values))) for _ in range(k)]
+        nums = [values[i] for i in indxs]
+        avg = sum(nums) / len(nums)
         mult = random.randint(1, 3)
-        indxs = [random.choice(range(len(values))) for i in range(k)]
         acquired_variables = [values[i] for i in indxs]
         for i in indxs:
-            values[i] = values[i] * mult
+            values[i] = avg * mult
+
+        mutex.release()
 
         message = f'Thread {thread_id}: Acquired variables: {acquired_variables}, avg: {avg}, multiplied: {values}\n'
-        file.write(message)
+        q.put(message)
 
-        time.sleep(random.randint(1, 5))
-
-    mutex.release()
-
-    file.flush()
+        sl_time = random.randint(1, 5)
+        time.sleep(sl_time)
+        print(f'Process {thread_id} will sleep for {sl_time}')
 
 
 if __name__ == '__main__':
@@ -42,21 +52,25 @@ if __name__ == '__main__':
     common_vars = [random.choice([i for i in range(10)]) for _ in range(m)]
     pid = 0
 
+    file_name = 'logs2.txt'
+    if os.path.getsize(file_name) > 0:
+        with open(file_name, 'w') as f:
+            pass
+
     mutex = mp.Lock()
     manager = mp.Manager()
+    q = mp.Queue()
     vars = manager.list(common_vars)
 
     for i in range(n):
         pid += 1
-        pr = mp.Process(target=thread_function, args=(pid, k, t, mutex, vars))
+        pr = mp.Process(target=thread_function, args=(pid, k, t, mutex, vars, q,))
         pr.start()
-        pr.join()
         jobs.append(pr)
 
     for proc in jobs:
         proc.join()
 
-    file.close()
-
-
-
+    logger_pr = mp.Process(target=logger, args=(q, file_name,))
+    logger_pr.start()
+    logger_pr.join()

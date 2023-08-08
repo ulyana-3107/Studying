e8a57f20d1363cae1f1144cc75a4c5348d16e520
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-08-08 18:49:02 +0300

    edited

--- Recursion/find_speed.py
+++ Recursion/find_speed.py
@@ -1,24 +1,44 @@
-def min_speed_rec(h: int, dist: list, curr):
-    if len(dist) == 0:
-        return 0
-    elif h < curr:
-        return -1
-    else:
-        s1 = dist[0] / (curr + 1)
-        s2 = min_speed_rec(h, dist[1:], curr + 1)
+import math
 
-        if s2 == -1:
-            return s1
+
+def calculate_time(speed, distances) -> int | float:
+    time = 0
+
+    for dist in distances:
+        time_taken = dist / speed
+        time += math.ceil(time_taken)
+
+    return time
+
+
+def optimal_speed(s1, s2, h, dist):
+    plus = (s2 - s1) / 2
+    time = calculate_time((s2 + plus) / 2, dist)
+
+    return time > h
+
+
+def find_best_speed(h: int, distances: list) -> int | float:
+    speed = sum(distances) / h
+    prev_speed = speed - 1
+
+    while True:
+        time1, time2 = calculate_time(prev_speed, distances), calculate_time(speed, distances)
+
+        if time2 == h:
+            return speed
+
+        elif time1 > h and time2 < h:
+            if optimal_speed(prev_speed, speed, h, distances):
+                return speed
+
+            else:
+                plus = (speed - prev_speed) / 2
+                prev_speed, speed = speed, speed + plus
         else:
-            return max(s1, s2)
-
-
-def find_speed(h: int, dist: list) -> float|int:
-    return min_speed_rec(h, dist, 1)
+            prev_speed, speed = speed, speed + 1
 
 
 if __name__ == '__main__':
-    r1 = find_speed(3, [1, 2, 3])
-    r2 = find_speed(5, [1, 2, 3, 4, 5])
-    r3 = find_speed(6, [5, 3, 8, 2, 4])
-    print(r1, r2, r3, sep='\n')
+    res = find_best_speed(3, [5, 3, 1])
+    print(res)

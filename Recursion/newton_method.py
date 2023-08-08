77086d1a525040f90c3774b47ae984a3a48fc1a2
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-08-08 17:10:30 +0300

    commmit

--- Recursion/newton_method.py
+++ Recursion/newton_method.py
@@ -4,30 +4,32 @@
 #  после n-кратного применения (5.8). После этого определите их асимптотическую стоимость вычисления.
 
 
-def linear_rec(a, x0, n) -> float:
+def tail_rec(a, x0, n, func):
     if n == 0:
         return x0
-
     else:
-        return linear_rec(a, x0 - (x0**2 - a)/(2*x0), n - 1)
+        return tail_rec(a, func(a, x0), n - 1, func)
 
 
-def tail_rec(a, x0, n, xn_1=None) -> float:
-    if n == 0:
-        return xn_1
+def func(a, x0):
+    return x0 - (x0**2 - a)*(2*x0)
 
-    else:
 
-        if xn_1 is None:
-            xn_1 = x0
-
-        xn = xn_1 - (xn_1 ** 2 - a)/(2*xn_1)
-
-        return tail_rec(a, x0, n - 1, xn)
+# def linear_rec(a, x0, n, xn_1=None) -> float:
+#     if n == 0:
+#         return xn_1
+#
+#     else:
+#
+#         if xn_1 is None:
+#             xn_1 = x0
+#
+#         xn = xn_1 - (xn_1 ** 2 - a)/(2*xn_1)
+#
+#         return linear_rec(a, x0, n - 1, xn)
 
 
 if __name__ == '__main__':
-    x0, a, n = 1.4, 2, 100
-    res1 = linear_rec(a, x0, n)
-    res2 = tail_rec(a, x0, n)
-    print(res1, res2, sep='\n')
+    x0, a, n = 1.4, 2, 5
+    res1 = tail_rec(a, x0, n, func)
+    print(res1)

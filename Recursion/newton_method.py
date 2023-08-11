74f7a416c5be71b57039d14696d7d6ac43108328
Ulasevich Ulyana <ulasevich.2003@inbox.ru>
2023-08-11 16:06:00 +0300

    edited

--- Recursion/newton_method.py
+++ Recursion/newton_method.py
@@ -4,35 +4,36 @@
 #  после n-кратного применения (5.8). После этого определите их асимптотическую стоимость вычисления.
 
 
-def func2(a, x0):
-    return x0 - (x0**2 - a)/(2*x0)
+# def func2(a, x0):
+#     return x0 - (x0**2 - a)/(2*x0)
 
 
-def func1(a, x0, n, func2):
+# def func1(a, x0, n, func2):
+#     if n == 0:
+#         return x0
+#     else:
+#         return func1(a, func2(a, x0), n - 1, func2)
+
+
+# def tail_rec(a, x0, n, func1, func2):
+#     return func1(a, x0, n, func2)
+
+
+def func(f1, f2, a, x0, n):
     if n == 0:
         return x0
-    else:
-        return func1(a, func2(a, x0), n - 1, func2)
+    return func(f1, f2, a, x0 - f1(x0) / f2(x0), n - 1)
 
 
-def tail_rec(a, x0, n, func1, func2):
-    return func1(a, x0, n, func2)
+def func1(x0):
+    pass
 
-# def linear_rec(a, x0, n, xn_1=None) -> float:
-#     if n == 0:
-#         return xn_1
-#
-#     else:
-#
-#         if xn_1 is None:
-#             xn_1 = x0
-#
-#         xn = xn_1 - (xn_1 ** 2 - a)/(2*xn_1)
-#
-#         return linear_rec(a, x0, n - 1, xn)
+
+def func2(x0):
+    pass
 
 
 if __name__ == '__main__':
     x0, a, n = 1.4, 2, 5
-    res1 = tail_rec(a, x0, n, func1, func2)
+    res1 = func(func1, func2, a, x0, n)
     print(res1)
